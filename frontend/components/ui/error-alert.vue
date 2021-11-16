f315fe2c88776a1c4dd85e732996a6583f8bdc55
Saad Siddiqui <saad.siddiqui@britecore.com>
2021-11-16 10:25:44 +0530

    refactor error-alert

--- frontend/components/ui/error-alert.vue
+++ frontend/components/ui/error-alert.vue
@@ -3,17 +3,7 @@
   v-if="allErrors.length"
   class="alert alert-error">
   <div class="flex-1">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      class="w-6 h-6 mx-2 stroke-current">
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
-    </svg>
+    <icon-slash class="w-6 h-6 mx-2 stroke-current"></icon-slash>
     <label
       v-for="(error, index) in allErrors"
       :key="index">{{ error.message }}</label>
@@ -25,9 +15,7 @@
 export default {
   name: 'ErrorAlert',
   props: {
-    errors: {
-      type: Object
-    }
+    errors: { type: Object, default: () => ({}) }
   },
   computed: {
     allErrors() {
