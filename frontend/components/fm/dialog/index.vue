793e8cc5e7019ffd5cc5e024105baf6708683194
Saad Siddiqui <saad.siddiqui@britecore.com>
2022-03-14 20:25:16 +0530

    implement dialog and confirm/alert services

---
+++ frontend/components/fm/dialog/index.vue
@@ -0,0 +1,109 @@
+<template>
+<transition enter-active-class="animatecss-fadeIn" leave-to-class="animatecss-fadeOut" @after-leave="$emit('hidden')">
+  <div v-show="isVisible" class="fm-dialog fm-dialog--sm animatecss" :class="classes">
+    <div class="fm-dialog__backdrop" @click="closeOnBackdropClick ? close() : () => {}"></div>
+    <transition enter-active-class="animatecss-zoomIn" leave-to-class="animatecss-zoomOut">
+      <div v-show="isVisible" class="fm-dialog__container animatecss" :class="dialogClass">
+        <div v-if="$slots.header" class="fm-dialog__header">
+          <slot name="header"></slot>
+        </div>
+
+        <button v-if="showClose" class="fm-dialog__close" @click="close">
+          <icon-x class="h-5 w-5"></icon-x>
+        </button>
+
+        <div class="fm-dialog__content">
+          <slot></slot>
+        </div>
+
+        <div v-if="$slots.footer" class="fm-dialog__footer">
+          <slot name="footer"></slot>
+        </div>
+      </div>
+    </transition>
+  </div>
+</transition>
+</template>
+
+<script>
+import { X as IconX } from 'lucide-vue';
+
+export default {
+  components: {
+    IconX
+  },
+  props: {
+    value: { type: Boolean, default: true },
+    showClose: { type: Boolean, default: true },
+    fullscreen: { type: Boolean, default: false },
+    alert: { type: Boolean, default: false },
+    closeOnBackdropClick: { type: Boolean, default: true },
+    dialogClass: { type: String, default: '' }
+  },
+  computed: {
+    isVisible: {
+      get() {
+        return this.value;
+      },
+      set(val) {
+        this.$emit('input', val);
+      }
+    },
+    classes() {
+      const { fullscreen, alert } = this;
+      return {
+        'fm-dialog--alert': alert,
+        'fm-dialog--clamped': !fullscreen,
+        'fm-dialog--fullscreen': fullscreen
+      };
+    }
+  },
+  methods: {
+    close() {
+      this.isVisible = false;
+    }
+  }
+};
+</script>
+<style lang="scss">
+.fm-dialog {
+  @apply z-20 fixed top-0 left-0 h-screen w-screen flex items-end md:items-center justify-center;
+  animation-duration: 200ms;
+}
+.fm-dialog__backdrop {
+  @apply absolute top-0 left-0 h-full w-full bg-black bg-opacity-50;
+}
+.fm-dialog__container {
+  @apply bg-white rounded-lg relative flex flex-col overflow-hidden;
+  animation-duration: 200ms;
+}
+.fm-dialog__header {
+  @apply px-6 py-4 border-b text-xl text-black font-medium pr-9;
+}
+.fm-dialog__close {
+  @apply absolute right-3 top-3 rounded-full flex-shrink-0 text-gray-500 hover:bg-gray-200 p-2 active:scale-90 transition-transform;
+}
+.fm-dialog__content {
+  @apply px-6 py-4 flex-grow overflow-auto;
+}
+.fm-dialog__footer {
+  @apply px-6 py-3 border-t;
+}
+
+
+.fm-dialog--clamped .fm-dialog__container {
+  @apply max-w-xl max-h-[90vh];
+}
+.fm-dialog--fullscreen .fm-dialog__container {
+  @apply w-full h-full rounded-none;
+}
+
+
+.fm-dialog--alert {
+  @apply items-center;
+  .fm-dialog__container {
+    @apply w-[28rem] max-w-[90vw];
+  }
+}
+
+</style>
