99156b523d1a982bc417426d4007039a3d1d807b
Saad Siddiqui <saad.siddiqui@britecore.com>
2022-03-25 02:14:35 +0530

    slightly revamp profile page

--- frontend/pages/profile.vue
+++ frontend/pages/profile.vue
@@ -1,112 +1,97 @@
 <template>
-<div v-if="user != null">
-  <div class="aspect-w-16 aspect-h-3 relative bg-black">
-    <img
-      class="object-cover h-full w-full absolute"
-      v-if="user.cover"
-      :src="user.cover.big"
-      alt="Cover photo">
-  </div>
-  <div class="-mt-6 container flex items-center">
-    <div class="avatar rounded-full p-1 bg-white">
-      <div class="w-32 h-32 rounded-full">
-        <img v-if="user.avatar" :src="user.avatar.medium">
-      </div>
-    </div>
-    <div class="ml-3">
-      <div class="text-2xl font-bold">{{ user.name || user.username }}</div>
-      <div class="flex items-center">
-        <icon-heart
-          class="mr-2 fill-current text-error"
-          :size="16"></icon-heart>
-        {{ user.follower_count }} followers
-      </div>
-    </div>
-    <button class="btn btn-black ml-auto" @click="toggleFollow">
-      {{ user.is_following ? 'Unfollow' : 'Follow' }}
-    </button>
-  </div>
+<div v-if="user">
+  <profile-above-the-tab></profile-above-the-tab>
+
   <div class="container">
-    <div class="my-6">
-      <div class="tabs">
-        <div class="tab tab-lg tab-lifted" :class="{ 'tab-active': activeTab === tabName.HOME }" @click="activeTab = tabName.HOME;">Home</div>
-        <div v-if="user.tiers.length" class="tab tab-lg tab-lifted" :class="{ 'tab-active': activeTab === tabName.TIERS }" @click="activeTab = tabName.TIERS;">Membership Tiers</div>
-        <div v-if="user.preferences.is_accepting_payments" class="tab tab-lg tab-lifted" :class="{ 'tab-active': activeTab === tabName.DONATIONS }" @click="activeTab = tabName.DONATIONS;">Donations</div>
-        <div class="tab tab-lg tab-lifted flex-grow cursor-default"></div>
-      </div>
-    </div>
-    <div v-show="activeTab == tabName.HOME" class="row pb-10">
-      <div class="col-7">
-        <div class="flex flex-wrap items-center">
-          <h1 class="text-2xl font-bold mr-auto">Posts</h1>
-          <button v-if="$auth.loggedIn && user.username == $auth.user.username" class="mt-4 sm:mt-0 btn btn-wide btn-black" @click="isAddPostVisible = true;">
-            <IconPlus class="mr-1" :size="16"></IconPlus>
-            Add a post
-          </button>
-        </div>
-        <div v-if="posts.count" class="mt-8 max-w-lg">
-          <post v-for="post in posts.results" :key="post.id" :post="post" class="mb-6"></post>
-        </div>
-      </div>
-      <div class="col"></div>
-      <div class="col-4">
-        <h1 class="text-2xl font-bold mr-auto">About</h1>
-        <div class="card shadow my-8">
-          <div class="card-body">
-            <p>{{ user.about || 'Nothing to see here' }}</p>
-            <div class="card-actions">
-              <a v-if="user.social_links.website_url" :href="user.social_links.website_url">
-                <icon-globe class="mr-1" :size="20"></icon-globe>
+    <fm-tabs v-model="activeTab" class="mt-8">
+      <fm-tabs-pane :id="tabName.HOME" label="Home" class="grid grid-cols-12 gap-5 pb-10">
+        <div class="col-span-12 md:col-span-7">
+          <fm-card body-class="bg-gray-100 text-gray-600" class="overflow-hidden">
+            <fm-read-more v-if="user.about" lines="6" class="mb-4">
+              <p v-html="user.about"></p>
+            </fm-read-more>
+            <div class="flex justify-center space-x-4 text-gray-600">
+              <a v-if="user.social_links.website_url" class="unstyled hover:text-gray-800" title="Website" target="_blank" :href="user.social_links.website_url">
+                <icon-globe :size="24"></icon-globe>
               </a>
-              <a v-if="user.social_links.youtube_url" :href="user.social_links.youtube_url">
-                <icon-youtube class="mr-1" :size="20"></icon-youtube>
+              <a v-if="user.social_links.twitter_url" class="unstyled hover:text-gray-800" title="Twitter" target="_blank" :href="user.social_links.twitter_url">
+                <icon-twitter :size="24"></icon-twitter>
               </a>
-              <a v-if="user.social_links.facebook_url" :href="user.social_links.facebook_url">
-                <icon-facebook class="mr-1" :size="20"></icon-facebook>
+              <a v-if="user.social_links.youtube_url" class="unstyled hover:text-gray-800" title="Youtube" target="_blank" :href="user.social_links.youtube_url">
+                <icon-youtube :size="24"></icon-youtube>
               </a>
-              <a v-if="user.social_links.instagram_url" :href="user.social_links.instagram_url">
-                <icon-instagram class="mr-1" :size="20"></icon-instagram>
+              <a v-if="user.social_links.instagram_url" class="unstyled hover:text-gray-800" title="Instagram" target="_blank" :href="user.social_links.instagram_url">
+                <icon-instagram :size="24"></icon-instagram>
               </a>
-              <a v-if="user.social_links.twitter_url" :href="user.social_links.twitter_url">
-                <icon-twitter class="mr-1" :size="20"></icon-twitter>
+              <a v-if="user.social_links.facebook_url" class="unstyled hover:text-gray-800" title="Facebook" target="_blank" :href="user.social_links.facebook_url">
+                <icon-facebook :size="24"></icon-facebook>
               </a>
+            </div>
+          </fm-card>
+
+          <div class="mt-8">
+            <div class="flex flex-wrap items-center">
+              <h1 class="text-2xl font-bold mr-auto">Posts</h1>
+              <button v-if="$auth.loggedIn && user.username == $auth.user.username" class="mt-4 sm:mt-0 btn btn-wide btn-black" @click="isAddPostVisible = true;">
+                <IconPlus class="mr-1" :size="16"></IconPlus>
+                Add a post
+              </button>
+            </div>
+            <div v-if="posts && posts.count" class="mt-8">
+              <post v-for="post in posts.results" :key="post.id" :post="post" class="mb-6"></post>
             </div>
           </div>
         </div>
-        <h1 class="text-2xl font-bold mr-auto">Recent Donations</h1>
-        <div class="row">
-          <donation v-for="donation in donations.results" :key="donation.id" :donation="donation">
-          </donation>
-        </div>
-      </div>
-    </div>
-    <add-post v-model="isAddPostVisible" @created="prependPost"></add-post>
-    <div v-show="activeTab == tabName.TIERS" class="row justify-center pb-10">
-      <tier
-        v-for="tier in user.tiers"
-        :key="tier.id"
-        :user="user"
-        :tier="tier"></tier>
-    </div>
-    <div v-show="activeTab == tabName.DONATIONS" class="row justify-end pb-10">
-      <div v-if="donations.count" class="col-12 md:col-6">
-        <div class="row justify-center">
-          <donation v-for="donation in donations.results" :key="donation.id" :donation="donation">
-          </donation>
-        </div>
-      </div>
-      <div class="col-12 md:col-6">
-        <div class="row justify-center">
+        <div class="col-span-12 md:col-span-5">
           <donation-form :user="user" @donated="prependDonation"></donation-form>
+          <div v-if="donations && donations.count">
+            <div>
+              <donation v-for="donation in donations.results" :key="donation.id" :donation="donation">
+              </donation>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </fm-tabs-pane>
+
+      <fm-tabs-pane :id="tabName.TIERS" label="Memberships" class="row justify-center pb-10">
+        <tier
+          v-for="tier in user.tiers"
+          :key="tier.id"
+          :user="user"
+          :tier="tier"></tier>
+      </fm-tabs-pane>
+
+      <fm-tabs-pane :id="tabName.DONATIONS" label="Donations" class="row justify-end pb-10">
+        <div v-if="donations && donations.count" class="col-12 md:col-6">
+          <div class="row justify-center">
+            <donation v-for="donation in donations.results" :key="donation.id" :donation="donation">
+            </donation>
+          </div>
+        </div>
+        <div class="col-12 md:col-6">
+          <div class="row justify-center">
+            <donation-form :user="user" @donated="prependDonation"></donation-form>
+          </div>
+        </div>
+      </fm-tabs-pane>
+    </fm-tabs>
   </div>
+
+
+  <add-post v-model="isAddPostVisible" @created="prependPost"></add-post>
 </div>
 </template>
 
 <script>
+import {
+  Plus as IconPlus
+} from 'lucide-vue';
+import { mapActions, mapGetters, mapState } from 'vuex';
+
 export default {
+  components: {
+    IconPlus
+  },
   layout:
     'default-no-container',
   props: {
@@ -122,9 +107,6 @@ export default {
       DONATIONS: 'donations'
     };
     return {
-      donations: {},
-      posts: {},
-      user: null,
       tabName,
       activeTab: tabName.HOME,
       isAddPostVisible: false
@@ -132,18 +114,18 @@ export default {
   },
   async fetch() {
     const username = this.username || this.$auth.user.username;
-    this.user = await this.$axios.$get(`/api/users/${username}/`);
-    this.posts = await this.$axios.$get(`/api/posts/?username=${username}`);
-    this.donations = await this.$axios.$get(`/api/donations/?username=${username}`);
+    await this.fetchProfile(username);
   },
   head: {
     title: 'My profile'
   },
+  computed: {
+    ...mapState('profile', ['user', 'donations', 'posts']),
+    ...mapGetters('profile', ['isSelfProfile'])
+  },
   methods: {
-    async toggleFollow() {
-      const action = this.user.is_following ? 'unfollow' : 'follow';
-      this.user = await this.$axios.$post(`/api/users/${this.user.username}/${action}/`);
-    },
+    ...mapActions('profile', ['fetchProfile']),
+
     prependPost(post) {
       this.posts.results.unshift(post);
     },
