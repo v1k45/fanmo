263b057c5745d0b74eea2e9e2b8a193c27d33783
Saad Siddiqui <saad.siddiqui@britecore.com>
2022-03-25 01:56:25 +0530

    implement popper component

---
+++ frontend/components/fm/popper/index.vue
@@ -0,0 +1,160 @@
+<template>
+<div>
+  <slot v-bind="slotProps"></slot>
+</div>
+</template>
+
+<script>
+import { createPopper } from '@popperjs/core';
+
+const registerClickOutside = function(el) {
+  el.clickOutsideEvent = function(event) {
+    if (el !== event.target && !el.contains(event.target)) {
+      el.dispatchEvent(new MouseEvent('click-outside', event));
+    }
+  };
+  document.body.addEventListener('mouseup', el.clickOutsideEvent);
+  document.body.addEventListener('touchend', el.clickOutsideEvent);
+};
+
+const unregisterClickOutside = function(el) {
+  document.body.removeEventListener('mouseup', el.clickOutsideEvent);
+  document.body.removeEventListener('touchend', el.clickOutsideEvent);
+  delete el.clickOutsideEvent;
+};
+
+export default {
+  props: {
+    placement: { type: String, default: 'bottom' },
+    toggleOnClick: { type: Boolean, default: true }
+  },
+  data() {
+    return {
+      slotProps: {
+        $reference: { 'data-popper-id': 'reference' },
+        $popper: { 'data-popper-id': 'popper' },
+        isVisible: false
+      },
+      popper: null,
+      state: {
+        referenceEl: null,
+        popperEl: null
+      }
+    };
+  },
+  computed: {
+  },
+  watch: {
+    'slotProps.isVisible'(isVisible) {
+      if (isVisible) this.show();
+      else this.hide();
+    },
+    placement() {
+      this.setOptions();
+    },
+    toggleOnClick() {
+      this.init({ force: true });
+    }
+  },
+  mounted() {
+    this.init();
+  },
+  updated() {
+    this.init();
+  },
+  beforeDestroy() {
+    this.destroy();
+  },
+  methods: {
+    getElements() {
+      if (!this.$el) return { reference: null, popper: null };
+      return {
+        reference: this.$el.querySelector(`[data-popper-id="${this.slotProps.$reference['data-popper-id']}"]`),
+        popper: this.$el.querySelector(`[data-popper-id="${this.slotProps.$popper['data-popper-id']}"]`)
+      };
+    },
+
+    init({ force = false } = {}) {
+      const { reference, popper } = this.getElements();
+      if (!force) {
+        if (this.state.referenceEl === reference && this.state.popperEl === popper) return;
+        if (!reference || !popper) return;
+      }
+      this.popper = createPopper(reference, popper, {
+        placement: this.placement,
+        modifiers: []
+      });
+      this.state = {
+        referenceEl: reference,
+        popperEl: popper
+      };
+
+      const showEvents = ['focus'];
+      const hideEvents = ['blur'];
+      if (this.toggleOnClick) {
+        registerClickOutside(reference);
+        showEvents.push('click');
+        hideEvents.push('click-outside');
+      } else {
+        showEvents.push('mouseenter');
+        hideEvents.push('mouseleave');
+      }
+
+      showEvents.forEach((event) => {
+        reference.addEventListener(event, () => {
+          this.slotProps.isVisible = true;
+        });
+      });
+
+      hideEvents.forEach((event) => {
+        reference.addEventListener(event, () => {
+          this.slotProps.isVisible = false;
+        });
+      });
+    },
+
+    setOptions() {
+      if (!this.popper) return;
+      this.popper.setOptions(options => ({
+        ...options,
+        placement: this.placement
+      }));
+    },
+
+    show() {
+      if (!this.popper) return;
+      // Enable the event listeners
+      this.popper.setOptions((options) => ({
+        ...options,
+        modifiers: [
+          ...options.modifiers,
+          { name: 'eventListeners', enabled: true }
+        ]
+      }));
+      this.slotProps.isVisible = true;
+      // Update its position
+      this.popper.update();
+    },
+
+    hide() {
+      if (!this.popper) return;
+      // Disable the event listeners
+      this.popper.setOptions((options) => ({
+        ...options,
+        modifiers: [
+          ...options.modifiers,
+          { name: 'eventListeners', enabled: false }
+        ]
+      }));
+      this.slotProps.isVisible = false;
+    },
+
+    destroy() {
+      if (!this.popper) return;
+      const { reference } = this.getElements();
+      if (reference) unregisterClickOutside(reference);
+      this.popper.destroy();
+    }
+  }
+};
+</script>
