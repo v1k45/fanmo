c8d049bb24c54fe5f2adad078cdbcf3d134be4c6
Saad Siddiqui <saad.siddiqui@britecore.com>
2022-03-25 01:57:08 +0530

    implement fm-dropdown-item

---
+++ frontend/components/fm/dropdown/item.vue
@@ -0,0 +1,20 @@
+<template>
+<button class="fm-dropdown__item" v-on="$listeners">
+  <slot></slot>
+</button>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss">
+.fm-dropdown__item {
+  @apply block w-full text-left p-3 cursor-pointer;
+  &:hover {
+    @apply bg-fm-primary-500 text-white;
+  }
+}
+</style>
