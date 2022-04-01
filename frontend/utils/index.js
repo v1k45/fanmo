4fe1521f9d80cc1596db5bf505a57afe12d50d41
Saad Siddiqui <saad.siddiqui@britecore.com>
2022-04-02 04:52:30 +0530

    remove global loading of rzp and implement utilities to do it on demand

--- frontend/utils/index.js
+++ frontend/utils/index.js
@@ -4,3 +4,19 @@ export const skipOnboarding = {
   set: (userId) => localStorage.setItem(`onboarding:skipped:${userId}`, '1'),
   unset: (userId) => localStorage.setItem(`onboarding:skipped:${userId}`, '0')
 };
+export const loadScript = url => {
+  const script = document.createElement('script');
+  script.src = url;
+  script.async = true;
+  document.body.appendChild(script);
+
+  return new Promise((resolve, reject) => {
+    script.onerror = reject;
+    script.onload = resolve;
+  });
+};
+
+export const loadRazorpay = () => {
+  if (window.Razorpay) return Promise.resolve();
+  return loadScript('https://checkout.razorpay.com/v1/checkout.js');
+};
