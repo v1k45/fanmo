39f26a6756c83b3f515795d0b13ff75e3f3ab238
Saad Siddiqui <saad.siddiqui@britecore.com>
2022-04-07 08:26:26 +0530

    use dedicated backgrounds

--- frontend/pages/profile.vue
+++ frontend/pages/profile.vue
@@ -2,53 +2,52 @@
 <div v-if="user">
   <profile-above-the-tab></profile-above-the-tab>
 
-  <div class="container">
-    <fm-tabs v-model="activeTab" centered class="mt-8">
-      <fm-tabs-pane :id="tabName.POSTS" lazy label="Posts" class="grid grid-cols-12 gap-5 pb-10">
-        <div class="col-span-12 md:col-span-7">
+  <fm-tabs v-model="activeTab" centered class="mt-8">
+    <fm-tabs-pane :id="tabName.POSTS" lazy label="Posts" class="grid grid-cols-12 gap-5 pb-10 container">
+      <div class="col-span-12 md:col-span-7">
 
-          <div class="mt-8">
-            <div class="flex flex-wrap items-center">
-              <h1 class="text-2xl font-bold mr-auto">Posts</h1>
-              <button v-if="$auth.loggedIn && user.username == $auth.user.username" class="mt-4 sm:mt-0 btn btn-wide btn-black" @click="isAddPostVisible = true;">
-                <IconPlus class="mr-1" :size="16"></IconPlus>
-                Add a post
-              </button>
-            </div>
-            <div v-if="posts && posts.count" class="mt-8">
-              <post v-for="post in posts.results" :key="post.id" :post="post" class="mb-6"></post>
-            </div>
+        <div class="mt-8">
+          <div class="flex flex-wrap items-center">
+            <h1 class="text-2xl font-bold mr-auto">Posts</h1>
+            <button v-if="$auth.loggedIn && user.username == $auth.user.username" class="mt-4 sm:mt-0 btn btn-wide btn-black" @click="isAddPostVisible = true;">
+              <IconPlus class="mr-1" :size="16"></IconPlus>
+              Add a post
+            </button>
+          </div>
+          <div v-if="posts && posts.count" class="mt-8">
+            <post v-for="post in posts.results" :key="post.id" :post="post" class="mb-6"></post>
           </div>
         </div>
-        <div class="col-span-12 md:col-span-5 h-full">
-          <fm-card body-class="bg-gray-100 text-gray-600" class="overflow-hidden sticky top-20">
-            <fm-read-more v-if="user.about" lines="6" class="mb-4">
-              <p v-html="user.about"></p>
-            </fm-read-more>
-            <div class="flex justify-center space-x-4 text-gray-600">
-              <a v-if="user.social_links.website_url" class="unstyled hover:text-gray-800" title="Website" target="_blank" :href="user.social_links.website_url">
-                <icon-globe :size="24"></icon-globe>
-              </a>
-              <a v-if="user.social_links.twitter_url" class="unstyled hover:text-gray-800" title="Twitter" target="_blank" :href="user.social_links.twitter_url">
-                <icon-twitter :size="24"></icon-twitter>
-              </a>
-              <a v-if="user.social_links.youtube_url" class="unstyled hover:text-gray-800" title="Youtube" target="_blank" :href="user.social_links.youtube_url">
-                <icon-youtube :size="24"></icon-youtube>
-              </a>
-              <a v-if="user.social_links.instagram_url" class="unstyled hover:text-gray-800" title="Instagram" target="_blank" :href="user.social_links.instagram_url">
-                <icon-instagram :size="24"></icon-instagram>
-              </a>
-              <a v-if="user.social_links.facebook_url" class="unstyled hover:text-gray-800" title="Facebook" target="_blank" :href="user.social_links.facebook_url">
-                <icon-facebook :size="24"></icon-facebook>
-              </a>
-            </div>
-          </fm-card>
-        </div>
-      </fm-tabs-pane>
+      </div>
+      <div class="col-span-12 md:col-span-5 h-full">
+        <fm-card body-class="bg-gray-100 text-gray-600" class="overflow-hidden sticky top-20">
+          <fm-read-more v-if="user.about" lines="6" class="mb-4">
+            <p v-html="user.about"></p>
+          </fm-read-more>
+          <div class="flex justify-center space-x-4 text-gray-600">
+            <a v-if="user.social_links.website_url" class="unstyled hover:text-gray-800" title="Website" target="_blank" :href="user.social_links.website_url">
+              <icon-globe :size="24"></icon-globe>
+            </a>
+            <a v-if="user.social_links.twitter_url" class="unstyled hover:text-gray-800" title="Twitter" target="_blank" :href="user.social_links.twitter_url">
+              <icon-twitter :size="24"></icon-twitter>
+            </a>
+            <a v-if="user.social_links.youtube_url" class="unstyled hover:text-gray-800" title="Youtube" target="_blank" :href="user.social_links.youtube_url">
+              <icon-youtube :size="24"></icon-youtube>
+            </a>
+            <a v-if="user.social_links.instagram_url" class="unstyled hover:text-gray-800" title="Instagram" target="_blank" :href="user.social_links.instagram_url">
+              <icon-instagram :size="24"></icon-instagram>
+            </a>
+            <a v-if="user.social_links.facebook_url" class="unstyled hover:text-gray-800" title="Facebook" target="_blank" :href="user.social_links.facebook_url">
+              <icon-facebook :size="24"></icon-facebook>
+            </a>
+          </div>
+        </fm-card>
+      </div>
+    </fm-tabs-pane>
 
-      <fm-tabs-pane :id="tabName.TIERS" lazy label="Memberships" class="row justify-center pb-10">
-
-        <div class="max-w-6xl">
+    <fm-tabs-pane :id="tabName.TIERS" lazy label="Memberships" class="pb-10 bg-gray-50">
+      <div class="container">
+        <div class="max-w-6xl mx-auto">
           <div class="row justify-center mt-2 gy-4 px-4">
             <div v-for="tier in user.tiers" :key="tier.id" class="col-12 md:col-6 lg:col-4">
               <profile-tier-card
@@ -59,7 +58,8 @@
             </div>
           </div>
         </div>
-      </fm-tabs-pane>
+      </div>
+    </fm-tabs-pane>
 
       <fm-tabs-pane :id="tabName.DONATION" lazy label="Donations" class="row max-w-6xl mx-auto bg-yellow-50 justify-end pb-10">
         <div v-if="donations && donations.count" class="col-12 md:col-7">
@@ -67,15 +67,11 @@
             <donation v-for="donation in donations.results" :key="donation.id" :donation="donation">
             </donation>
           </div>
-        </div>
-        <div class="col-12 md:col-5">
-          <div class="row justify-center">
-            <donation-form :user="user" @donated="prependDonation"></donation-form>
           </div>
         </div>
-      </fm-tabs-pane>
-    </fm-tabs>
-  </div>
+      </div>
+    </fm-tabs-pane>
+  </fm-tabs>
 
 
   <add-post v-model="isAddPostVisible" @created="prependPost"></add-post>
