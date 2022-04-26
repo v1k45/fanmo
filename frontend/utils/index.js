50aad163d4ac1f445f710116e3a3319e458c808e
dumptyd <dumptyd2.0@gmail.com>
2022-04-26 17:08:16 +0530

    create clipboard and social sharing utilities

--- frontend/utils/index.js
+++ frontend/utils/index.js
@@ -29,3 +29,67 @@ export const loadRazorpay = () => {
   if (window.Razorpay) return Promise.resolve();
   return loadScript('https://checkout.razorpay.com/v1/checkout.js');
 };
+
+/**
+* We use shorter names to reduce the final bundle size
+*
+* Properties:
+* @url = url
+* @text = title
+* @hashtags = hashtags
+*/
+const socialSharingSchema = Object.freeze({
+  email: 'mailto:?subject=@text&body=@url',
+  facebook: 'https://www.facebook.com/sharer/sharer.php?u=@url&title=@text&hashtag=@hashtags',
+  reddit: 'https://www.reddit.com/submit?url=@url&title=@text',
+  telegram: 'https://t.me/share/url?url=@url&text=@text',
+  twitter: 'https://twitter.com/intent/tweet?text=@text&url=@url&hashtags=@hashtags',
+  whatsapp: 'https://api.whatsapp.com/send?text=@text%0D%0A@url'
+});
+
+export const getSocialLink = (social, { url, text }) => {
+  let template = socialSharingSchema[social];
+  if (!template) throw new Error('Unsupported social');
+  template = template.replace('@url', encodeURIComponent(url));
+  template = template.replace('@text', encodeURIComponent(text));
+  template = template.replace('@hashtags', encodeURIComponent('fanmo'));
+  return template;
+};
+
+
+/**
+ * Copies the provided text to the clipboard.
+ * @param {string} text - text to copy to the clipboard
+ * @returns {Promise} A promise that gets resolved or rejected based on whether the text was successfully copied to the clipboard
+ */
+export const copy = (() => {
+  // modified from here: https://stackoverflow.com/a/30810322
+
+  const fallbackCopyTextToClipboard = text => {
+    const textarea = document.createElement('textarea');
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+
+    let success = false;
+    try {
+      success = document.execCommand('copy');
+    } catch (err) {
+      success = false;
+    }
+
+    document.body.removeChild(textarea);
+    return success;
+  };
+
+  const copyTextToClipboard = text => {
+    if (!navigator.clipboard) {
+      const success = fallbackCopyTextToClipboard(text);
+      // eslint-disable-next-line prefer-promise-reject-errors
+      return success ? Promise.resolve() : Promise.reject();
+    }
+    return navigator.clipboard.writeText(text);
+  };
+
+  return copyTextToClipboard;
+})();
