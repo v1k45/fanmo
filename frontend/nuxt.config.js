b9d5bcf3ab92d213656f6f07286d7f564f2fa41e
dumptyd <dumptyd2.0@gmail.com>
2022-04-28 01:13:54 +0530

    add meta tag to fix broken fixed positioned mobile dialogs

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -21,7 +21,8 @@ export default {
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      // https://stackoverflow.com/questions/44679794/position-fixed-on-chrome-mobile-causing-element-to-move-on-scroll-up-down
+      { name: 'viewport', content: 'height=device-height, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, target-densitydpi=device-dpi' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
