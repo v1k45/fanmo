cdedf6680121a862b53fc86a9067090d81a70cb5
dumptyd <dumptyd2.0@gmail.com>
2022-05-05 09:25:35 +0530

    revamp supporter donations page

--- frontend/pages/sent-donations.vue
+++ frontend/pages/sent-donations.vue
@@ -1,85 +1,238 @@
+
 <template>
-<div v-if="donations != null">
-  <div>
-    <div class="mt-8 flex max-w-md">
-      <div class="flex-grow mr-2"><fm-input v-model="filter.search" placeholder="Search by name or email"></fm-input></div>
-      <div><fm-button type="primary" @click="loadDonations">Search</fm-button></div>
+<div class="mt-6 lg:my-12 lg:mx-4">
+
+  <!-- header start -->
+  <div class="container sm:pl-0">
+    <div class="text-2xl text-black font-bold">
+      <template v-if="$auth.user.is_creator">My</template> Donations
     </div>
-    <div class="mt-8 flex max-w-md items-center">
-      <div class="flex-grow mr-2">
-        <fm-input v-model="filter.orderBy" type="select" label="Sort by">
-          <option value="-created_at">Newest first</option>
-          <option value="created_at">Oldest first</option>
+    <div class="mt-1 text-gray-600">Search and view your donations.</div>
+  </div>
+  <!-- header end -->
+
+  <!-- container start -->
+  <div v-if="donations" class="container mt-5 pt-6 pb-16 md:px-8 rounded-xl border bg-white">
+
+    <!-- filters start -->
+    <div class="flex flex-wrap w-full">
+
+      <!-- search start -->
+      <div class="mr-auto lg:max-w-sm flex-grow">
+        <label class="text-sm block font-bold mb-2">Search for donations</label>
+        <fm-input v-model="filter.search" placeholder="Search by creator name" @input="handleSearchInput">
+          <template #prepend>
+            <icon-search class="w-em"></icon-search>
+          </template>
+        </fm-input>
+      </div>
+      <!-- search end -->
+
+      <div class="w-full mt-4 lg:hidden"></div>
+
+      <!-- sort start -->
+      <div class="w-full sm:w-[unset] lg:ml-4">
+        <label class="text-sm block font-bold mb-2">Sort by</label>
+        <fm-input v-model="filter.orderBy" type="select" @change="loadDonations">
+          <option value="-created_at">Newest donation first</option>
+          <option value="created_at">Oldest donation first</option>
+          <option value="-amount">Highest donation first</option>
+          <option value="amount">Lowest donation first</option>
           <option value="-lifetime_amount">Highest lifetime amount first</option>
           <option value="lifetime_amount">Lowest lifetime amount first</option>
         </fm-input>
       </div>
-      <div><fm-button type="primary" @click="loadDonations">Sort</fm-button></div>
+      <!-- sort end -->
+
     </div>
-  </div>
-  <table class="table table-auto">
-    <thead>
-      <tr>
-        <th>Creator</th>
-        <th>Amount</th>
-        <th>Message</th>
-        <th>Lifetime amount</th>
-        <th>Date</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="donation in donations.results" :key="donation.id">
-        <td>{{ donation.creator_user.display_name }}</td>
-        <td>{{ donation.amount }}</td>
-        <td class="truncate">{{ donation.message }}</td>
-        <td>{{ donation.lifetime_amount }}</td>
-        <td>{{ donation.created_at }}</td>
-        <td><fm-button type="info" @click="viewDonation(donation)">view</fm-button></td>
-      </tr>
-    </tbody>
-  </table>
-  <fm-button v-if="donations.next" type="info" @click="fetchMoreDonations(donations.next)">Load More</fm-button>
-  <fm-dialog v-if="activeDonation" v-model="showDialog">
-    <template #header>{{ activeDonation.creator_user.display_name }}</template>
-    <dl>
-      <dt>Amount</dt>
-      <dd>
-        {{ activeDonation.amount }}
-      </dd>
-      <dt>Message</dt>
-      <dd>{{ activeDonation.message }}</dd>
-      <dt>Date</dt>
-      <dd>{{ activeDonation.created_at }}</dd>
-      <dt>Lifetime amount</dt>
-      <dd>{{ activeDonation.lifetime_amount }}</dd>
-    </dl>
-    <h3>Payment History</h3>
-    <table v-if="payments" class="table table-auto">
+    <!-- filters end -->
+
+    <!-- table start -->
+    <fm-table class="mt-8 table-fixed lg:table-auto" first-column-sticky>
+      <colgroup>
+        <col class="w-[100px] md:min-w-[200px]"> <!-- creator -->
+        <col class="min-w-[120px]"> <!-- amount -->
+        <col> <!-- message -->
+        <col class="min-w-[140px]"> <!-- lifetime amount -->
+        <col class="min-w-[175px]"> <!-- date -->
+        <col class="w-[100px]"> <!-- actions -->
+      </colgroup>
       <thead>
-        <tr>
-          <td>Date</td>
-          <td>Amount</td>
-          <td>Transaction ID</td>
-          <td>Payout</td>
+        <tr class="text-xs uppercase">
+          <th>Creator</th>
+          <th class="!text-right">Amount</th>
+          <th>Message</th>
+          <th class="!text-right">Lifetime amount</th>
+          <th>Date</th>
+          <th></th>
         </tr>
       </thead>
-      <tbody>
-        <tr v-for="payment in payments.results" :key="payment.id">
-          <td>{{ payment.created_at }}</td>
-          <td>{{ payment.amount }}</td>
-          <td>{{ payment.external_id }}</td>
-          <td v-if="payment.payout">{{ payment.payout.amount }} {{ payment.payout.status }}</td>
-          <td v-else>Pending</td>
+      <tbody v-show="donations.results.length" class="text-sm">
+        <tr v-for="donation in donations.results" :key="donation.id">
+          <th>
+            <div class="flex items-center">
+              <fm-avatar
+                :src="donation.creator_user.avatar && donation.creator_user.avatar.small"
+                :name="donation.creator_user.display_name" :username="donation.creator_user.username"
+                size="w-5 h-5 lg:w-7 lg:h-7 mr-2 inline-block font-normal flex-shrink-0">
+              </fm-avatar>
+              <div class="max-w-[100px] lg:max-w-[200px] font-normal">
+                <div class="truncate text-xs lg:text-sm text-black" :title="donation.creator_user.display_name">{{ donation.creator_user.display_name }}</div>
+                <div v-if="donation.creator_user.one_liner" class="hidden lg:block truncate text-xs font-normal text-gray-500">{{ donation.creator_user.one_liner }}</div>
+              </div>
+            </div>
+          </th>
+          <td class="text-right">{{ $currency(donation.amount) }}</td>
+          <td>
+            <div class="truncate max-w-[200px]">
+              <span v-if="donation.message && donation.is_hidden" title="Message is only visible to you and the creator.">
+                <icon-eye-off class="w-4 h-4 text-gray-400 mr-1"></icon-eye-off>
+              </span>
+              <span :title="donation.message">{{ donation.message }}</span>
+            </div>
+          </td>
+          <td class="text-right">{{ $currency(donation.lifetime_amount) }}</td>
+          <td>{{ $datetime(donation.created_at) }}</td>
+          <td class="text-center">
+            <fm-button size="sm" @click="viewDonation(donation)">Details</fm-button>
+          </td>
         </tr>
       </tbody>
-    </table>
-    <fm-button v-if="payments && payments.next" type="info" @click="fetchMorePayments(payments.next)">Load More</fm-button>
+    </fm-table>
+    <div v-if="!donations.results.length" class="flex items-center justify-center min-h-[200px] bg-gray-50 rounded-b-lg border border-t-0">
+      <div class="text-sm">
+        <template v-if="hasActiveFilters">No results matched your filtering criteria.</template>
+        <template v-else>You haven't made any donations yet.</template>
+      </div>
+    </div>
+    <!-- table end -->
+
+    <!-- next button start -->
+    <div v-if="donations.next" class="text-center mt-4">
+      <fm-button :loading="isNextDonationsLoading" @click="fetchMoreDonationsLocal(donations.next)">Load More</fm-button>
+    </div>
+    <!-- next button end -->
+  </div>
+  <!-- container end -->
+
+  <!-- dialogs start -->
+  <fm-dialog v-model="showDialog" drawer>
+    <template #header>
+      <div v-if="activeDonation" class="text-base">
+        Donation details for {{ activeDonation.creator_user.display_name }}
+      </div>
+    </template>
+
+    <template v-if="activeDonation">
+
+      <!-- name, one liner and avatar start -->
+      <div class="flex items-center">
+        <fm-avatar
+          :src="activeDonation.creator_user.avatar && activeDonation.creator_user.avatar.small"
+          :name="activeDonation.creator_user.display_name" :username="activeDonation.creator_user.username"
+          size="w-8 h-8 mr-2 inline-block font-normal flex-shrink-0">
+        </fm-avatar>
+        <div>
+          <div class="text-black" :title="activeDonation.creator_user.display_name">{{ activeDonation.creator_user.display_name }}</div>
+          <div v-if="activeDonation.creator_user.one_liner" class="text-xs font-normal text-gray-500">
+            {{ activeDonation.creator_user.one_liner }}
+          </div>
+        </div>
+      </div>
+      <!-- name, one liner and avatar end -->
+
+      <hr class="my-4">
+
+      <!-- key value pairs start -->
+      <dl>
+
+        <!-- date start -->
+        <div class="flex items-baseline mb-2">
+          <dt class="text-sm text-gray-500 w-1/3 mr-2 md:w-1/4">Date</dt>
+          <dd class="flex-grow">{{ $datetime(activeDonation.created_at) }}</dd>
+        </div>
+        <!-- date end -->
+
+        <!-- amount start -->
+        <div class="flex items-baseline mb-2">
+          <dt class="text-sm text-gray-500 w-1/3 mr-2 md:w-1/4">Amount</dt>
+          <dd class="flex-grow">{{ $currency(activeDonation.amount) }}</dd>
+        </div>
+        <!-- amount end -->
+
+        <!-- lifetime amount start -->
+        <div class="flex items-baseline mb-2">
+          <dt class="text-sm text-gray-500 w-1/3 mr-2 md:w-1/4">Lifetime amount</dt>
+          <dd class="flex-grow">{{ $currency(activeDonation.lifetime_amount) }}</dd>
+        </div>
+        <!-- lifetime amount end -->
+
+        <!-- message visibility start -->
+        <div v-if="activeDonation.message" class="flex items-baseline mb-2">
+          <dt class="text-sm text-gray-500 w-1/3 mr-2 md:w-1/4">Message visibility</dt>
+          <dd class="flex items-center flex-grow flex-wrap">
+            <div class="mr-auto">{{ activeDonation.is_hidden ? 'Only visible to you and the creator' : 'Visible to everyone' }}</div>
+          </dd>
+        </div>
+        <!-- message visibility end -->
+
+
+        <!-- message start -->
+        <div class="mb-2">
+          <dt class="text-sm text-gray-500">Message <icon-corner-right-down class="h-em w-em"></icon-corner-right-down></dt>
+          <dd class="border rounded-lg p-3 mt-4">
+            <fm-read-more-height v-if="activeDonation.message" max-height="200">
+              <div class="whitespace-pre-wrap overflow-auto">{{ activeDonation.message }}</div>
+            </fm-read-more-height>
+            <div v-else class="text-gray-600 text-sm italic">No message was included with this donation.</div>
+          </dd>
+        </div>
+        <!-- message end -->
+
+      </dl>
+      <!-- key value pairs end -->
+
+      <hr class="my-4">
+
+      <!-- payment history table start -->
+      <h3 class="font-medium">Payment History <template v-if="payments && payments.count">({{ payments.count }})</template></h3>
+      <fm-table class="mt-4">
+        <thead>
+          <tr class="text-xs uppercase">
+            <th>Date</th>
+            <th class="!text-right">Amount</th>
+            <th>Method</th>
+            <th>Transaction ID</th>
+          </tr>
+        </thead>
+        <tbody v-if="payments" class="text-sm">
+          <tr v-for="payment in payments.results" :key="payment.id">
+            <td><span class="whitespace-nowrap">{{ $datetime(payment.created_at) }}</span></td>
+            <td class="text-right">{{ $currency(payment.amount) }}</td>
+            <td>{{ payment.method }}</td>
+            <td><code class="text-xs">{{ payment.external_id }}</code></td>
+          </tr>
+        </tbody>
+      </fm-table>
+      <div v-if="!payments || !payments.results.length" class="flex items-center justify-center min-h-[200px] bg-gray-50 rounded-b-lg border border-t-0">
+        <div class="text-sm">You haven't made any donation transactions yet.</div>
+      </div>
+      <!-- payment history table end -->
+
+      <!-- nuxt button start -->
+      <div v-if="payments && payments.next" class="text-center mt-4">
+        <fm-button size="sm" :loading="isNextPaymentsLoading" @click="fetchMorePaymentsLocal(payments.next)">Load More</fm-button>
+      </div>
+      <!-- nuxt button end -->
+
+    </template>
   </fm-dialog>
+  <!-- dialogs end -->
 </div>
 </template>
 
 <script>
+import debounce from 'lodash/debounce';
 import { mapActions, mapState } from 'vuex';
 
 export default {
@@ -90,21 +243,29 @@ export default {
         search: '',
         orderBy: '-created_at'
       },
-      showDialog: true,
-      activeDonation: null
+      showDialog: false,
+      activeDonation: null,
+      isNextDonationsLoading: false,
+      isNextPaymentsLoading: false
     };
   },
   head: {
-    title: 'Received Donations'
+    title: 'Sent Donations'
   },
   computed: {
-    ...mapState('donations', ['donations', 'payments'])
+    ...mapState('donations', ['donations', 'payments']),
+    hasActiveFilters() {
+      return !!(this.filter.search);
+    }
   },
   mounted() {
     this.loadDonations();
   },
   methods: {
     ...mapActions('donations', ['fetchSentDonations', 'fetchMoreDonations', 'fetchPayments', 'fetchMorePayments']),
+    handleSearchInput: debounce(function() {
+      this.loadDonations();
+    }, 250),
     loadDonations() {
       const params = { fan_username: this.$auth.user.username, ordering: this.filter.orderBy };
       if (this.filter.search) {
@@ -116,6 +277,16 @@ export default {
       this.activeDonation = donation;
       this.showDialog = true;
       this.fetchPayments({ relatedDonationId: donation.id });
+    },
+    async fetchMoreDonationsLocal(url) {
+      this.isNextDonationsLoading = true;
+      await this.fetchMoreDonations(url);
+      this.isNextDonationsLoading = false;
+    },
+    async fetchMorePaymentsLocal(url) {
+      this.isNextPaymentsLoading = true;
+      await this.fetchMorePayments(url);
+      this.isNextPaymentsLoading = false;
     }
   }
 };
