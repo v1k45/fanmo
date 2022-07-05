1c8265d0742968df02a4c7d4d7839f7cda6ebfcf
Vikas <v1k45x@gmail.com>
2022-07-05 05:47:11 +0530

    Fix deployment script to use dockerhub auth

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Script to pull in new code and restarting the containers
 
+set -o errexit
+set -o pipefail
+set -o nounset
+
 git pull
-sudo docker stack deploy --compose-file dev.yml memberships
+sudo docker stack deploy --compose-file dev.yml memberships --with-registry-auth
