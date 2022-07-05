c8bc9efc4000eab1ec2c6734354aae74a7d1c12e
Vikas <v1k45x@gmail.com>
2022-07-05 05:34:28 +0530

    update deploy script to use docker swarm

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 # Script to pull in new code and restarting the containers
 
-sudo docker-compose -f dev.yml stop
 git pull
-sudo docker-compose -f dev.yml pull
-sudo docker-compose -f dev.yml build
-sudo docker-compose -f dev.yml up -d
+sudo docker stack deploy --compose-file dev.yml memberships
