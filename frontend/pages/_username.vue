5ed7d9512a2dc0e69cfe50c5f32203cbbe43026d
dumptyd <dumptyd2.0@gmail.com>
2022-07-11 04:00:31 +0530

    empty state for donations (self and public)

--- frontend/pages/_username.vue
+++ frontend/pages/_username.vue
@@ -133,15 +133,22 @@
     <fm-tabs-pane :id="tabName.DONATION" lazy label="Donations" class="bg-gray-50 pb-10">
       <div class="container min-h-[300px]">
         <div class="row gx-0 lg:gx-4 max-w-6xl mx-auto justify-center">
-          <div v-if="donations && donations.length" class="col-12 order-2 lg:col-7 lg:order-1">
+          <div v-if="donations" class="col-12 order-2 lg:col-7 lg:order-1">
             <hr class="mt-6 mb-8 lg:hidden">
-            <div class="text-xl font-bold mb-4">Recent donations ({{ donations.length }})</div>
+            <div class="text-xl font-bold mb-4">Recent donations</div>
             <fm-lazy v-for="(donation, idx) in donations" :key="donation.id"
               :class="{ 'mb-4 lg:mb-6': idx !== donations.length - 1 }" min-height="100">
               <profile-donation :donation="donation">
               </profile-donation>
             </fm-lazy>
-            <div v-if="!donations.length" class="mt-8 text-gray-500">Nothing to see here yet.</div>
+            <div v-if="!donations.length" class="text-gray-500 text-center max-w-md mx-auto my-24">
+              <template v-if="isSelfProfile">
+                Your recent donations will show up here.
+              </template>
+              <template v-else>
+                Become the first one to show your support for {{ user.display_name }}. Your contribution will show up here.
+              </template>
+            </div>
           </div>
           <div class="col-12 order-1 mb-6 sm:col-10 md:col-8 lg:col-5 lg:order-2 lg:mb-0">
             <donation-widget
