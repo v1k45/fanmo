2ae79a946bc67f81b10427094f74c58fb3fd4798
dumptyd <dumptyd2.0@gmail.com>
2022-07-16 05:29:45 +0530

    update v-loader ui yet again

--- frontend/components/misc/loading.vue
+++ frontend/components/misc/loading.vue
@@ -1,7 +1,9 @@
 <template>
 <div class="fixed z-30 top-0 left-0 h-full w-full flex flex-col justify-center items-center bg-white bg-opacity-80">
-  <icon-loader-2 class="select-none animate-spin h-8 w-8"></icon-loader-2>
-  <div v-if="text" class="mt-3 text-black">{{ text }}</div>
+  <div class="animate-pulse">
+    <logo circle class="h-16 w-16"></logo>
+  </div>
+  <div v-if="text" class="mt-3 text-lg text-black">{{ text }}</div>
 </div>
 </template>
 
