40e37706cb6c7fa11e2e9afe5cf4c3c03dd09655
dumptyd <dumptyd2.0@gmail.com>
2022-07-21 01:31:17 +0530

    implement and unregister unused components

---
+++ frontend/components/fm/list/index.vue
@@ -0,0 +1,19 @@
+<template>
+<ul class="fm-list" :class="{ 'fm-list--actionable': actionable }">
+  <slot></slot>
+</ul>
+</template>
+
+<script>
+export default {
+  props: {
+    actionable: { type: Boolean, default: false }
+  }
+};
+</script>
+
+<style lang="scss">
+.fm-list {
+  @apply border-t;
+}
+</style>
---
+++ frontend/components/fm/list/item.vue
@@ -0,0 +1,23 @@
+<template>
+<li class="fm-list__item" :class="{ 'fm-list__item--actionable': actionable }">
+  <slot></slot>
+</li>
+</template>
+
+<script>
+export default {
+  props: {
+    actionable: { type: Boolean, default: false }
+  }
+};
+</script>
+<style lang="scss">
+.fm-list__item {
+  @apply flex items-center border-l border-r border-b bg-white px-4 py-3;
+}
+
+.fm-list__item--actionable,
+.fm-list--actionable > .fm-list__item {
+  @apply cursor-pointer hover:bg-gray-200;
+}
+</style>
