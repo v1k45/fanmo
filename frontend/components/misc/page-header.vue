090053ae835bd6b15651e3aa50015ec717fb0987
dumptyd <dumptyd2.0@gmail.com>
2022-07-23 23:42:53 +0530

    implement page header component

---
+++ frontend/components/misc/page-header.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+  <div class="text-2xl text-black font-bold">{{ (page.supporterTitle && !$auth.user.is_creator) ? page.supporterTitle : page.title }}</div>
+  <div class="mt-1 text-gray-600">{{ page.description }}</div>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    page: { type: Object, required: true }
+  }
+};
+</script>
