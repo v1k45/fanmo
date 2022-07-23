96e49a0de29e03cbb734183fe8f9a785bd2497d1
Vikas <v1k45x@gmail.com>
2022-07-23 23:14:47 +0530

    Fix missing padding on mobile onboarding flow

--- frontend/layouts/auth-empty.vue
+++ frontend/layouts/auth-empty.vue
@@ -1,5 +1,5 @@
 <template>
-<main class="sm:py-16 bg-gray-50 min-h-screen">
+<main class="sm:py-16 py-8 bg-gray-50 min-h-screen">
   <logo class="h-8 mx-auto"></logo>
   <fm-card class="max-w-lg mx-auto py-4 sm:px-4 mt-12 relative">
     <Nuxt></Nuxt>
