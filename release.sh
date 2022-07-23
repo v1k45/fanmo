ca44803180a002212f1da50d49f89dee0bcdb0ca
Vikas <v1k45x@gmail.com>
2022-07-23 08:03:48 +0530

    Drop dev suffix on builds

--- release.sh
+++ release.sh
@@ -10,11 +10,11 @@ eval $(bumpver show -n --env)
 BUILD_TIMESTAMP=$( date '+%F %H:%M:%S' )
 REPO="v1k45/fanmo:"
 
-DEV_TAG="${REPO}${CURRENT_VERSION}-dev"
-DEV_LATEST="${REPO}latest-dev"
+TAG="${REPO}${CURRENT_VERSION}"
+LATEST="${REPO}latest"
 DOCKER_BUILDKIT=0
 COMPOSE_DOCKER_CLI_BUILD=0
-docker build -f compose/production/django/Dockerfile -t "$DEV_TAG" -t "$DEV_LATEST" --build-arg VERSION="$CURRENT_VERSION" --build-arg BUILD_TIMESTAMP="$BUILD_TIMESTAMP" --build-arg BUILD_ENVIRONMENT=local .
-docker push "$DEV_LATEST"
-docker push "$DEV_TAG" 
+docker build -f compose/production/django/Dockerfile -t "$TAG" -t "$LATEST" --build-arg VERSION="$CURRENT_VERSION" --build-arg BUILD_TIMESTAMP="$BUILD_TIMESTAMP" .
+docker push "$LATEST"
+docker push "$TAG" 
 git push origin master --tags
