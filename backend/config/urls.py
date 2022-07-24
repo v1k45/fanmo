1b6af76d58d2962bbd1b01682c8ef701dcaa8880
Vikas <v1k45x@gmail.com>
2022-07-24 06:24:25 +0530

    Use index view for django error handling

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,8 +3,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
-from django.views import defaults as default_views
-from django.views.generic import TemplateView
 from fanmo.core.views import index_view, page_view, post_view
 
 
@@ -51,30 +49,14 @@ urlpatterns = [
     # Your stuff: custom urls includes go here
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+handler404 = 'fanmo.core.views.handle_404'
+handler403 = 'fanmo.core.views.handle_403'
+handle400 = 'fanmo.core.views.handle_404'
+handle500 = 'fanmo.core.views.handle_500'
+
 if settings.DEBUG:
     # Static file serving when using Gunicorn + Uvicorn for local web socket development
     urlpatterns += staticfiles_urlpatterns()
-
-    # This allows the error pages to be debugged during development, just visit
-    # these url in browser to see how these error pages look like.
-    urlpatterns += [
-        path(
-            "400/",
-            default_views.bad_request,
-            kwargs={"exception": Exception("Bad Request!")},
-        ),
-        path(
-            "403/",
-            default_views.permission_denied,
-            kwargs={"exception": Exception("Permission Denied")},
-        ),
-        path(
-            "404/",
-            default_views.page_not_found,
-            kwargs={"exception": Exception("Page not Found")},
-        ),
-        path("500/", default_views.server_error),
-    ]
     if "debug_toolbar" in settings.INSTALLED_APPS:
         import debug_toolbar
 
