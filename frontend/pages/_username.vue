e90bedbb1bec7fd366740de2735eee1d34592f4d
dumptyd <dumptyd2.0@gmail.com>
2022-08-10 07:45:41 +0530

    fix empty state spacing for tiers

--- frontend/pages/_username.vue
+++ frontend/pages/_username.vue
@@ -151,7 +151,7 @@
           <!-- no tier action start -->
           <fm-card
             v-if="!user.tiers.length"
-            class="mx-auto max-w-xl overflow-hidden" body-class="text-center !pt-16 !pb-20">
+            class="mx-auto max-w-xl overflow-hidden mt-6" body-class="text-center !pt-16 !pb-20">
             <icon-crown class="h-16 w-16 stroke-1 animatecss animatecss-tada"></icon-crown>
             <div class="mt-2">
               Make a steady monthly income while offering exclusive content and experience based on membership tiers to your fans.
